7e60d9ac79ced9541614849565a652c4803e94d8
curious725 <curious725@gmail.com>
2018-02-06 10:23:36 +0200

    register app people in installed apps

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -51,6 +51,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'people.apps.PeopleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
