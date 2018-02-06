895591ab351a9a41c4588a3ca9cec2042109e374
curious725 <curious725@gmail.com>
2018-02-06 21:19:32 +0000

    add ContactForm

---
+++ src/people/forms.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ValidationError
+from django import forms
+from .models import Contact
+
+def validate_name(name):
+    if len(name) < 2:
+        raise ValidationError('Ensure this value has at least 2 characters')
+
+        
+def validate_phone(phone):
+    if len(phone) < 5 or len(phone) > 13:
+        raise ValidationError('Ensure this value has at least 5 digits and less than 14 digits')
+        
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(
+        validators=[validate_name]
+    )
+    phone = forms.CharField(
+        validators=[validate_phone],
+        widget=forms.TextInput(
+            attrs={'pattern': '[0-9]{5-13}', 'title': 'Enter digits only'}
+        )
+    )
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone']
