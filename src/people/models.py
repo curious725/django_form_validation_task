175686ff0544b85b65fbbb46390c637597214761
curious725 <curious725@gmail.com>
2018-02-06 11:04:16 +0200

    add model Contact

--- src/people/models.py
+++ src/people/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+    email = models.EmailField(
+        unique=True
+    )
+    phone = models.CharField(
+        null=True,
+        blank=True,
+        max_length=13,
+        unique=True
+    )
