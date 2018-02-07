42310f9801bde618f988f4afd1707b74dd00be95
curious725 <curious725@gmail.com>
2018-02-07 11:33:15 +0200

    improve form validation

--- src/people/forms.py
+++ src/people/forms.py
@@ -2,15 +2,22 @@ from django.core.exceptions import ValidationError
 from django import forms
 from .models import Contact
 
+
 def validate_name(name):
     if len(name) < 2:
         raise ValidationError('Ensure this value has at least 2 characters')
 
-        
+
 def validate_phone(phone):
+    try:
+        int(phone)
+    except ValueError:
+        raise ValidationError('Phone number can include only digits.')
     if len(phone) < 5 or len(phone) > 13:
-        raise ValidationError('Ensure this value has at least 5 digits and less than 14 digits')
-        
+        raise ValidationError(
+            'Ensure this value has at least 5 digits and less than 14 digits'
+        )
+
 
 class ContactForm(forms.ModelForm):
     name = forms.CharField(
