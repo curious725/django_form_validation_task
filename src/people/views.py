11377b687623e7287d5904bf74c37b543c601276
curious725 <curious725@gmail.com>
2018-02-07 11:32:22 +0200

    modify new_contact view

--- src/people/views.py
+++ src/people/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
+
 
 def index(request):
     contacts = Contact.objects.all()
@@ -7,5 +9,16 @@ def index(request):
         request, 'people/index.html', {'contacts': contacts}
     )
 
+
 def new_contact(request):
-    pass
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            contact.save()
+            return redirect('index')
+    else:
+        form = ContactForm()
+    return render(
+        request, 'people/new_contact.html', {'form': form}
+    )
